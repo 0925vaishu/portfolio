5eb5f75971f9fdf4bfae214de0cc659b8fae484a
“Vaishnavi <polvaishnavi2001@gmail.com>
2025-08-19 16:18:36 +0530

    modilication

--- src/fontawesome.jsx
+++
@@ -1,8 +0,0 @@
-import '@fortawesome/fontawesome-svg-core';
-import { config } from "@fortawesome/fontawesome-svg-core";
-
-config.autoAddCss = false
-
-export default function FontAwesomeConfig() {
-    return null
-}
